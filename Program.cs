f9107b3727f6d2bfb8defc93a75e025fe636dd78
feridmzd <fmemmedzade60@gmail.com>
2024-03-11 13:02:49 -0700

    arr added

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//int[] brr = { 223, 34, 377,1,  4};
+
+
+//int tek=0;
+//int cut=0;
+//for(int i = 0; i < brr.Length; i++)
+//{
+//    if (brr[i] % 2 == 0)
+//    {
+//        cut += 1;
+//    }
+//    else {
+//        tek++;
+//    }
+
+//}
+//Console.WriteLine(tek);
+
+
+
+//int[] arr ={ 223, 34, 377, 1, 4 };
+//int max = 0;
+//int max2 = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] > max)
+//        max = arr[i];
+
+
+//    for (int j = 0; j < arr.Length; j++)
+//    {
+//        if (arr[j] < max && arr[j] > max2)
+//        {
+//            max2 = arr[j];
+//        }
+//    }
+//}
+
+//Console.WriteLine(max);
